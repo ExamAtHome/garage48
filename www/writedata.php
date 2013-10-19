ed248fa24298580d5377585ca79ffd803ef33e24
Andrey Bogdanov <bay73@mail.ru>
2013-10-19 11:44:21 +0400

    JSON prettyfing

--- www/writedata.php
+++ www/writedata.php
@@ -1,6 +1,60 @@
 <?php
 $id = $_REQUEST['id'];
-$data = $_REQUEST['data'];
+function prettyPrint( $json )
+{
+    $result = '';
+    $level = 0;
+    $prev_char = '';
+    $in_quotes = false;
+    $ends_line_level = NULL;
+    $json_length = strlen( $json );
+
+    for( $i = 0; $i < $json_length; $i++ ) {
+        $char = $json[$i];
+        $new_line_level = NULL;
+        $post = "";
+        if( $ends_line_level !== NULL ) {
+            $new_line_level = $ends_line_level;
+            $ends_line_level = NULL;
+        }
+        if( $char === '"' && $prev_char != '\\' ) {
+            $in_quotes = !$in_quotes;
+        } else if( ! $in_quotes ) {
+            switch( $char ) {
+                case '}': case ']':
+                    $level--;
+                    $ends_line_level = NULL;
+                    $new_line_level = $level;
+                    break;
+
+                case '{': case '[':
+                    $level++;
+                case ',':
+                    $ends_line_level = $level;
+                    break;
+
+                case ':':
+                    $post = " ";
+                    break;
+
+                case " ": case "\t": case "\n": case "\r":
+                    $char = "";
+                    $ends_line_level = $new_line_level;
+                    $new_line_level = NULL;
+                    break;
+            }
+        }
+        if( $new_line_level !== NULL ) {
+            $result .= PHP_EOL.str_repeat( "   ", $new_line_level );
+        }
+        $result .= $char.$post;
+        $prev_char = $char;
+    }
+
+    return $result;
+}
+
+$data = prettyPrint($_REQUEST['data']);
 $file = 'data/data'.$id.'.json';
 file_put_contents($file, $data);
 ?>
